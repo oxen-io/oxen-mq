b11d2870bd825215b7c0cd9321809d6368dc7f04
Jason Rhinelander <jason@imaginary.ca>
2020-02-13 00:53:43 -0400

    Fix verify pubkey initialization

--- lokimq/lokimq.cpp
+++ lokimq/lokimq.cpp
@@ -372,7 +372,7 @@ LokiMQ::LokiMQ(
         // Verify the pubkey.  We could get by with taking just the privkey and just generate this
         // for ourselves, but this provides an extra check to make sure we and the caller agree
         // cryptographically (e.g. to make sure they don't pass us an ed25519 keypair by mistake)
-        std::string verify_pubkey{crypto_box_PUBLICKEYBYTES, 0};
+        std::string verify_pubkey(crypto_box_PUBLICKEYBYTES, 0);
         crypto_scalarmult_base(reinterpret_cast<unsigned char*>(&verify_pubkey[0]), reinterpret_cast<unsigned char*>(&privkey[0]));
         if (verify_pubkey != pubkey)
             throw std::invalid_argument("Invalid pubkey/privkey values given to LokiMQ construction: pubkey verification failed");
