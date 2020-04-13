b081cf9331c7e2b651d20bc694a96d383b2180e1
Jason Rhinelander <jason@imaginary.ca>
2020-04-13 16:11:30 -0300

    Add missing SET_SNS proxy handler

--- lokimq/proxy.cpp
+++ lokimq/proxy.cpp
@@ -256,6 +256,8 @@ void LokiMQ::proxy_control_message(std::vector<zmq::message_t>& parts) {
             LMQ_TRACE("proxy batch jobs");
             auto ptrval = bt_deserialize<uintptr_t>(data);
             return proxy_batch(reinterpret_cast<detail::Batch*>(ptrval));
+        } else if (cmd == "SET_SNS") {
+            return proxy_set_active_sns(data);
         } else if (cmd == "UPDATE_SNS") {
             return proxy_update_active_sns(data);
         } else if (cmd == "CONNECT_SN") {
