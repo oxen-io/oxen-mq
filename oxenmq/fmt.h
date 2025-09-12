d669b06356f466688c36ea9ef8dbd6f064fdf32a
Jason Rhinelander <jason@imaginary.ca>
2025-09-12 18:12:07 -0300

    Fix fmt-related build issues
    - Need to set C++20 mode earlier
    - Drop unneeded export
    - Fix oxen::logging dependency
    - fmt 11 formatting compat fix

--- oxenmq/fmt.h
+++ oxenmq/fmt.h
@@ -8,19 +8,19 @@
 
 template <>
 struct fmt::formatter<oxenmq::AuthLevel> : fmt::formatter<std::string_view> {
-    auto format(oxenmq::AuthLevel v, format_context& ctx) {
+    auto format(oxenmq::AuthLevel v, format_context& ctx) const {
         return formatter<std::string_view>::format(to_string(v), ctx);
     }
 };
 template <>
 struct fmt::formatter<oxenmq::ConnectionID> : fmt::formatter<std::string> {
-    auto format(const oxenmq::ConnectionID& conn, format_context& ctx) {
+    auto format(const oxenmq::ConnectionID& conn, format_context& ctx) const {
         return formatter<std::string>::format(conn.to_string(), ctx);
     }
 };
 template <>
 struct fmt::formatter<oxenmq::address> : fmt::formatter<std::string> {
-    auto format(const oxenmq::address& addr, format_context& ctx) {
+    auto format(const oxenmq::address& addr, format_context& ctx) const {
         return formatter<std::string>::format(addr.full_address(), ctx);
     }
 };
